d6e66719d31916791cd7cd0f4c075dbf612dba8f
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-05-31 18:38:46 +0700

    fix:source for cms

--- .source/index.ts
+++ .source/index.ts
@@ -1,9 +1,11 @@
 // @ts-nocheck -- skip type checking
-import * as work_0 from "../content/works/test-outstatic-cms.mdx?collection=work&hash=1748666422426"
-import * as blog_2 from "../content/blogs/my-go-to-tools-for-wireframing-and-prototyping.mdx?collection=blog&hash=1748666422426"
-import * as blog_1 from "../content/blogs/how-to-create-a-design-system-from-scratch.mdx?collection=blog&hash=1748666422426"
-import * as blog_0 from "../content/blogs/5-ux-principles-every-designer-should-know.mdx?collection=blog&hash=1748666422426"
+import * as work_2 from "../content/works/travelly-travel-booking-app.mdx?collection=work&hash=1748689938854"
+import * as work_1 from "../content/works/shopease-e-commerce-web-redesign.mdx?collection=work&hash=1748689938854"
+import * as work_0 from "../content/works/fintrack-personal-finance-dashboard.mdx?collection=work&hash=1748689938854"
+import * as blog_2 from "../content/blogs/my-go-to-tools-for-wireframing-and-prototyping.mdx?collection=blog&hash=1748689938854"
+import * as blog_1 from "../content/blogs/how-to-create-a-design-system-from-scratch.mdx?collection=blog&hash=1748689938854"
+import * as blog_0 from "../content/blogs/5-ux-principles-every-designer-should-know.mdx?collection=blog&hash=1748689938854"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
 export const blog = _runtime.doc<typeof _source.blog>([{ info: {"path":"5-ux-principles-every-designer-should-know.mdx","absolutePath":"D:/side-projects/personal-blog/content/blogs/5-ux-principles-every-designer-should-know.mdx"}, data: blog_0 }, { info: {"path":"how-to-create-a-design-system-from-scratch.mdx","absolutePath":"D:/side-projects/personal-blog/content/blogs/how-to-create-a-design-system-from-scratch.mdx"}, data: blog_1 }, { info: {"path":"my-go-to-tools-for-wireframing-and-prototyping.mdx","absolutePath":"D:/side-projects/personal-blog/content/blogs/my-go-to-tools-for-wireframing-and-prototyping.mdx"}, data: blog_2 }]);
-export const work = _runtime.doc<typeof _source.work>([{ info: {"path":"test-outstatic-cms.mdx","absolutePath":"D:/side-projects/personal-blog/content/works/test-outstatic-cms.mdx"}, data: work_0 }]);
+export const work = _runtime.doc<typeof _source.work>([{ info: {"path":"fintrack-personal-finance-dashboard.mdx","absolutePath":"D:/side-projects/personal-blog/content/works/fintrack-personal-finance-dashboard.mdx"}, data: work_0 }, { info: {"path":"shopease-e-commerce-web-redesign.mdx","absolutePath":"D:/side-projects/personal-blog/content/works/shopease-e-commerce-web-redesign.mdx"}, data: work_1 }, { info: {"path":"travelly-travel-booking-app.mdx","absolutePath":"D:/side-projects/personal-blog/content/works/travelly-travel-booking-app.mdx"}, data: work_2 }]);
