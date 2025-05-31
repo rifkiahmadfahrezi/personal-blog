9a590da859ca0f5949e1e5d4d014ba0da2d5c7d8
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-05-31 18:38:23 +0700

    feat: Add site configuration for metadata in layout

---
+++ src/config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Ethan Walker",
+  description:
+    "a UI/UX designer passionate about creating clean, intuitive, and user-centered digital experiences. Explore design projects, case studies, blog posts, and more — showcasing work in web and mobile design, prototyping, and user research.",
+}
